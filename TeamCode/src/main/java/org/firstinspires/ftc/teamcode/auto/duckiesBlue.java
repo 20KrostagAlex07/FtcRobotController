5c0bf49a21586e78de5a7614738522e79e6aac0d
Sam948-byte <techguy763@gmail.com>
2022-01-15 09:29:14 -0600

    BACKUP 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/duckiesBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/duckiesBlue.java
@@ -37,26 +37,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This file illustrates the concept of driving a path based on time.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backwards for 1 Second
- *   - Stop and close the claw.
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @Autonomous(name="Ducks Blue", group="Autonomous", preselectTeleOp = "Field Red")
 

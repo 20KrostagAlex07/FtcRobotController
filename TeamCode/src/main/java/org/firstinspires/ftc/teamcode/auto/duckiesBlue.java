d8f580e080af0420ebca618680d75d5bd2c6a630
Sam Freund <techguy763@gmail.com>
2022-02-03 21:11:45 -0600

    eliminate wrist auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/duckiesBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/duckiesBlue.java
@@ -47,10 +47,10 @@ public class duckiesBlue extends LinearOpMode {
     private DcMotor arm;
     private DcMotor duckies;
     private BNO055IMU imu;
-    private Servo wrist;
+
     private Servo grabber;
 
-    private float wristPos = 150;
+
     private float grabberPos = 30;
 
     public DcMotor frontLeft;
@@ -77,7 +77,7 @@ public class duckiesBlue extends LinearOpMode {
         duckies = hardwareMap.get(DcMotor.class, "duckies");
         imu = hardwareMap.get(BNO055IMU.class, "gyro");
         grabber = hardwareMap.get(Servo.class, "grabber");
-        wrist = hardwareMap.get(Servo.class, "wrist");
+      
         frontLeft = hardwareMap.get(DcMotor.class, "front_left");
         frontRight = hardwareMap.get(DcMotor.class, "front_right");
         backLeft = hardwareMap.get(DcMotor.class, "back_left");
@@ -126,7 +126,7 @@ public class duckiesBlue extends LinearOpMode {
 
         // Step 1:  Drive forward for 3 seconds
 
-      wrist.setPosition(0);
+
 
 
       set(100, 100, 100, 100, 0.5);

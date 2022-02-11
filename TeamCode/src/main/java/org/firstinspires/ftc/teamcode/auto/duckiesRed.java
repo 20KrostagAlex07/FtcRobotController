1fe5b7b5194eb674d47e2b3a7ec7b93273f68119
Sam948-byte <techguy763@gmail.com>
2022-02-10 20:08:22 -0600

    duckiesRed auto mode is being remade

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/duckiesRed.java
@@ -0,0 +1,193 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name = "Ducks Red", group = "Autonomous", preselectTeleOp = "Field Generic")
+
+public class duckiesRed extends LinearOpMode {
+
+
+    private final ElapsedTime runtime = new ElapsedTime();
+    private DcMotor arm;
+    private DcMotor duckies;
+    private BNO055IMU imu;
+
+    private Servo grabber;
+
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    private final boolean isA = false;
+    private final boolean wasA = false;
+    private final int i = 0;
+    private final int dir = -1;
+    private final double previousHeading = 0;
+    private final double integratedHeading = 0;
+    private final BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        arm = hardwareMap.get(DcMotor.class, "arm1");
+        duckies = hardwareMap.get(DcMotor.class, "duckies");
+        imu = hardwareMap.get(BNO055IMU.class, "gyro");
+        grabber = hardwareMap.get(Servo.class, "grabber");
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+
+        //reverse stuff
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        //set zeropowerbehavior
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //reset encoder
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //run to position
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        grabber.setPosition(0.8);
+        sleep(2000);
+        grabber.setPosition(0.1);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
+
+        // Step 1:  Drive forward for 3 seconds
+
+        // Move Back
+        set(-10, 10, -10, 10, 0.5);
+        sleep(200);
+
+        brake();
+        sleep(10000);
+
+        // Move Right to Wall
+        set(50, 50, -50, -50, 0.5);
+        sleep(500);
+
+        brake();
+        sleep(10000);
+
+        // Move Forward
+        set(20, -20, 20, -20, 0.5);
+        sleep(450);
+
+        brake();
+        sleep(10000);
+
+        // Spin the Wheel
+        duckies.setPower(-0.6);
+        sleep(4000);
+
+        duckies.setPower(0);
+
+        // Move left for a little bit
+        set(-25, -25, 25, 25, 0.5);
+        sleep(500);
+
+        brake();
+        sleep(10000);
+
+        // Move back into Shipping Area
+        set(-50, 50, -50, 50, 0.5);
+        sleep(500);
+
+        brake();
+        sleep(10000);
+
+    }
+
+    private double set(int fL, int fR, int bL, int bR, double pwr) {
+        frontLeft.setTargetPosition(fL);
+        frontRight.setTargetPosition(fR);
+        backLeft.setTargetPosition(bL);
+        backRight.setTargetPosition(bR);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(pwr);
+        frontRight.setPower(pwr);
+        backLeft.setPower(pwr);
+        backRight.setPower(pwr);
+
+        return 0;
+    }
+
+    private void brake() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+    }
+}

50d13c63dcfb7095022aaa0f9377c50b9d724556
20KrostagAlex07 <95828530+20KrostagAlex07@users.noreply.github.com>
2022-02-23 21:08:18 -0600

    Update armClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armClass.java
@@ -1,5 +1,6 @@
-package org.firstinspires.ftc.teamcode.teleop;
+//No errors except for the before-stated argument issues with "armPID"
 
+package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,32 +8,50 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 
+//make class for PID
 public class armClass {
-
 	
-	 //construct armPID 
-	 @param kP Proportional coefficient  
-	 @param kI Integral coefficient 
-	 @param kD Derivative coefficient 
-	 
-	public armPID(double kP, double kI, double kD) {
-
+	
+	/**
+	* @param kP - proportional value
+	* @param kI - integral value
+	* @param kD - deriative value
+	*/
+	
+	public void armPID(double kP, double kI, double kD) {
+			
 	}
 
 	
 	 //update the PID controller output
-	 @param target
-	 @param state
 	 // @return the command to our motor, I.E. motor power 
 	 
-	public double armControl(double target, double state) {
+	public void armControl(double target, double state) {
 		// PID logic and then return the output 
+		
+		 //initialize stuff
+		 double error = target - state;
+		 double errorLast = 0;
+		 ElapsedTime timer = new ElapsedTime();
+		 
+		 //PID logic
+		 error = target - state;
+		 
+		 double outP = kP * error;
+		 double outI = 0;
+		 double outD = kD * ((error - errorLast) / timer.seconds());
+		 double update = outP + outI + outD; 
+			 
+		 //reset stuff
+		 errorLast = error;
+		 timer.reset();
+	}
+}
 		
 		 //initialize stuff
 		 double errorLast;
